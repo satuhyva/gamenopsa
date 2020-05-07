d1f4d624745739382a6aef6fd2e29193b4da1478
Satu Hyvärinen <hyvarinen.satu.i@gmail.com>
2020-05-07 17:20:38 +0300

    setting up continues

--- src/App.js
+++ src/App.js
@@ -1,27 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          xxxxxxxxxx   Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import { View, Dimensions,  StyleSheet, Text } from 'react-native'
+// import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+// import NopsaGame from './components/NopsaGame'
+// import rootReducer from './reducers/rootReducer'
+
+
+// const store = createStore(rootReducer)
+
+// const App = () => {
+//     return (
+//         <Provider store={store}>
+//             <NopsaGame/>
+//         </Provider>
+//     )
+// }
+
+// export default App
+
+
+const App = () => {
+    return (
+        <View>
+            <Text>HUHUUUUUUUUU</Text>
+            <Text>HUHUUUUUUUUU</Text>
+            <Text>HUHUUUUUUUUU</Text>
+            <Text>HUHUUUUUUUUU</Text>
+            <Text>HUHUUUUUUUUU</Text>
+            <Text>HUHUUUUUUUUU</Text>
+        </View>
+    )
 }
 
-export default App;
+export default App
+
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           xxxxxxxxxx   Edit <code>src/App.js</code> and save to reload.
+//         </p>
+        
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
