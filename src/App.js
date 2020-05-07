3c34c2b6c3aa92e77ab87d192866a6c933561aed
Satu Hyvärinen <hyvarinen.satu.i@gmail.com>
2020-05-07 17:17:53 +0300

    setting up continues

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          OMAAAAAAAAA   Edit <code>src/App.js</code> and save to reload.
+          xxxxxxxxxx   Edit <code>src/App.js</code> and save to reload.
         </p>
         
         <a
