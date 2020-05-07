33b36e1bd83faa2a03d65ab978f567814f81b857
Satu Hyvärinen <hyvarinen.satu.i@gmail.com>
2020-05-07 17:13:03 +0300

    Updates

---
+++ precache-manifest.64deccbde61248278b81d6698308835a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad7cb01cb7e394e55012ab5bf95b0c8",
+    "url": "/gamenopsa/index.html"
+  },
+  {
+    "revision": "fc716468aed092ab92e0",
+    "url": "/gamenopsa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6e4c00e03933c67d92dc",
+    "url": "/gamenopsa/static/js/2.b03d1131.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gamenopsa/static/js/2.b03d1131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc716468aed092ab92e0",
+    "url": "/gamenopsa/static/js/main.c13f2351.chunk.js"
+  },
+  {
+    "revision": "a280dce322faa66dea8b",
+    "url": "/gamenopsa/static/js/runtime-main.5995c17c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gamenopsa/static/media/logo.5d5d9eef.svg"
+  }
+]);
