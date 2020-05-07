a0a58b3c2475a1ef13666188126f769b9e57b5a0
Satu Hyvärinen <hyvarinen.satu.i@gmail.com>
2020-05-07 18:41:10 +0300

    setting up continues

--- src/components/setup/SetUpGame.js
+++ src/components/setup/SetUpGame.js
@@ -7,24 +7,10 @@ import { setGameSettings } from '../../reducers/gameReducer'
 
 const SetUpGame = (props) => {
 
-    const screenWidth = Dimensions.get('window').width
-    const screenHeight = Dimensions.get('window').height
-    // const cardWidthIfScreenWidthIsUsed = screenWidth / 6
-    // const cardWidthIfScreenHeightIsUsed = screenHeight / ((0.5 + 1 + 0.75 + 1 + 0.75 + 1.5 + 0.5) * 1.7)
-    // const cardWidth = Math.min(cardWidthIfScreenWidthIsUsed, cardWidthIfScreenHeightIsUsed)
-    // const cardHeight = 1.7 * cardWidth
-    // const spacing = (screenWidth - 6 * cardWidth) / 2
-
-    const styles = getStyles(screenWidth, screenHeight)
-
 
     return (
-        // <View style={styles.titleView}>
-        //     <Text style={styles.titleText}>nopsa</Text>
-        //     <Text style={styles.infoText}>a card game for those with skill, speed and luck</Text>
-        // </View>
-        <View style={styles.screen}>
-            <Text>huhuuu</Text>
+        <View >
+            <Text>huhuuu SETUPPOII</Text>
         </View>
     )
 }
@@ -44,32 +30,6 @@ const ConnectedSetUpGame = connect(mapStateToProps, mapDispatchToProps)(SetUpGam
 export default ConnectedSetUpGame
 
 
-
-const getStyles = (screenWidth, screenHeight) => {
-    return StyleSheet.create({
-        screen: {
-            alignItems: 'center',
-            width: screenWidth,
-            height: screenHeight,
-            backgroundColor: 'green',
-        },
-        // titleView: {
-        //     height: screenWidth / 10,
-        //     alignItems: 'center',
-        // },
-        // titleText: {
-        //     fontSize: screenWidth / 10,
-        //     fontWeigth: 'bold',
-        //     color: '#B9CC3F',
-        //     fontFamily: 'Arial Black',
-        // },
-        // infoText: {
-        //     fontSize: screenWidth / 20,
-        //     color: '#B9CC3F',
-        //     fontFamily: 'Arial',
-        // },
-    })
-}
 
 
 
