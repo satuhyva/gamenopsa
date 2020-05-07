d7f3f94c11f24177fa4a303a16cb11d7ebeec30b
Satu Hyvärinen <hyvarinen.satu.i@gmail.com>
2020-05-07 18:48:59 +0300

    Updates

---
+++ precache-manifest.db8aa2673438f4e3d1dd42a56501b82e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73319cefb2e36f57229227c52aac3b9",
+    "url": "/gamenopsa/index.html"
+  },
+  {
+    "revision": "462f7e16d54aac234c4d",
+    "url": "/gamenopsa/static/js/2.eabe0d80.chunk.js"
+  },
+  {
+    "revision": "066a3b5f5d30e9efcd56434ebe4db621",
+    "url": "/gamenopsa/static/js/2.eabe0d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6f78f307315e652208f",
+    "url": "/gamenopsa/static/js/main.f10e55ad.chunk.js"
+  },
+  {
+    "revision": "a280dce322faa66dea8b",
+    "url": "/gamenopsa/static/js/runtime-main.5995c17c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gamenopsa/precache-manifest.05438ecef723a14c06210b4f26fef999.js"
+  "/gamenopsa/precache-manifest.db8aa2673438f4e3d1dd42a56501b82e.js"
 );
 
 self.addEventListener('message', (event) => {
