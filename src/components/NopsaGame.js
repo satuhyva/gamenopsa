ac3fea365c76ab12daaaeb1945dfdaf20f61faa9
Satu Hyvärinen <hyvarinen.satu.i@gmail.com>
2020-05-07 18:13:44 +0300

    setting up continues

--- src/components/NopsaGame.js
+++ src/components/NopsaGame.js
@@ -5,40 +5,31 @@ import SetUpNewGame from './setupGame/SetUpNewGame'
 import Game from './game/Game'
 
 
-const NopsaGame = (props) => {
-    console.log(props.game)
-    const arvo = props.game.isOn
-    return (
-        <View>
-            <Text>nopsa game</Text>
-            <Text>muutos</Text>
-            <Text>{arvo ? 'totta' : 'valhetta'}</Text>
-        </View>
-    )
-}
-
-
-
-// export default NopsaGame
-
-// import React from 'react'
-// import { View, Text } from 'react-native'
-// import { connect } from 'react-redux'
-// import SetUpNewGame from './setupGame/SetUpNewGame'
-// import Game from './game/Game'
-
-
 // const NopsaGame = (props) => {
+//     console.log(props.game)
+//     const arvo = props.game.isOn
 //     return (
 //         <View>
-//             {!props.game.gameIsOn ?
-//                 <SetUpNewGame/>
-//                 :
-//                 <Game/>
-//             }
+//             <Text>nopsa game</Text>
+//             <Text>muutos</Text>
+//             <Text>{arvo ? 'totta' : 'valhetta'}</Text>
 //         </View>
 //     )
 // }
+
+
+const NopsaGame = (props) => {
+    const gameIsOn = props.game.gameIsOn
+    return (
+        <View>
+            {!gameIsOn ?
+                <SetUpNewGame/>
+                :
+                <Game/>
+            }
+        </View>
+    )
+}
 
 const mapStateToProps = state => {
     return {
